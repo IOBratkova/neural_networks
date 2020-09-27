ae3bc6749ace6e62540d60b58c7ab59a3830b0a3
Браткова Ирина Олеговна <bratkova.io@reliab.tech>
2020-09-27 22:42:43 +0300

    отрисовка поля для ввода буков (по координатам в поле Canvas)

---
+++ nw1/classes/grid.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+class MyGrid:
+    def __init__(self, rows, cols, widget):
+        self.widget = widget
+        self.count_rows = rows
+        self.count_cols = cols
+        self.titles = [[None for _ in range(self.count_cols)] for _ in range(self.count_rows)]
+        self.grid = Canvas(self.widget, width=250, height=250, borderwidth=5, background='white')
+
+    def set_gird(self):
+        self.grid.pack(anchor=NW)
+        self.grid.bind("<Button-1>", self.call_back)
+
+    def call_back(self, event):
+        col_width = self.grid.winfo_width() / self.count_cols
+        row_height = self.grid.winfo_height() / self.count_rows
+        col = int(event.x // col_width)
+        row = int(event.y // row_height)
+        if not self.titles[row][col]:
+            self.titles[row][col] = self.grid.create_rectangle(col * col_width, row * row_height, (col + 1) * col_width,
+                                                 (row + 1) * row_height, fill="black")
+        else:
+            self.grid.delete(self.titles[row][col])
+            self.titles[row][col] = None
+
