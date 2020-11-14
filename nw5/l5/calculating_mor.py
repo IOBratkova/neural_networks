8058c0b1aefb7034dc67de9b88fdbd9d2a8de0de
Браткова Ирина <bratkova.io@reliab.tech>
2020-11-14 09:26:09 +0300

    ЛР4: структура нс

---
+++ nw5/l5/calculating_mor.py
@@ -0,0 +1,59 @@
+from calculate.utils import *
+
+
+class CalculatingMor:
+    def __init__(self):
+        self.letter_a1 = None
+        self.letter_a2 = None
+        self.letter_b1 = None
+        self.letter_b2 = None
+        self.letter_rs = None
+        self.m_list = None
+
+    def teaching(self):
+        print('\n>> ОБУЧЕНИЕ')
+        self.__make_letters_list()
+        print('\n>> Создание обучающих выборок')
+        self.m_list = self.__make_all_m()
+
+    def __make_letters_list(self):
+        self.letters_list = [
+            (self.letter_a1, self.letter_a2),
+            (self.letter_b1, self.letter_b2)]
+
+    def __make_all_m(self):
+        def __upd_binary_list():
+            result = []
+            for el in self.letters_list:
+                tmp1 = copy_and_insert_one(el[0])
+                tmp2 = copy_and_insert_one(el[1])
+                result.append((tmp1, tmp2))
+            return result
+
+        def __make_m(index):
+            result = []
+            lst = __upd_binary_list()
+            for i in range(len(lst)):
+                if i == index:
+                    tmp1 = lst[i][0], 1
+                    tmp2 = lst[i][1], 1
+                    result.append(tmp1)
+                    result.append(tmp2)
+                    print(tmp1)
+                    print(tmp2)
+                else:
+                    tmp1 = lst[i][0], 0
+                    tmp2 = lst[i][1], 0
+                    result.append(tmp1)
+                    result.append(tmp2)
+                    print(tmp1)
+                    print(tmp2)
+            return result
+
+        res = []
+        for i in range(len(self.letters_list)):
+            j = i+1
+            print('m-' + str(j))
+            tmp = __make_m(i)
+            res.append(tmp)
+        return res
---
+++ nw5/l5/controller_mor.py
@@ -0,0 +1,21 @@
+from l5.calculating_mor import CalculatingMor
+
+
+class ControllerMor:
+    def __init__(self):
+        self.algorithm = CalculatingMor()
+
+    def read_letter_a1(self, matrix):
+        self.algorithm.letter_a1 = [0 if not el else 1 for line in matrix for el in line]
+
+    def read_letter_a2(self, matrix):
+        self.algorithm.letter_a2 = [0 if not el else 1 for line in matrix for el in line]
+
+    def read_letter_b1(self, matrix):
+        self.algorithm.letter_b1 = [0 if not el else 1 for line in matrix for el in line]
+
+    def read_letter_b2(self, matrix):
+        self.algorithm.letter_b2 = [0 if not el else 1 for line in matrix for el in line]
+
+    def read_letter_rs(self, matrix):
+        self.algorithm.letter_rs = [0 if not el else 1 for line in matrix for el in line]
